3f214d75fb711fe7623401187023044cce887bb3
VickyIr <vicky.ir@students.amikom.ac.id>
2023-04-03 20:37:38 +0700

    Initial Commit

---
+++ Itambah/ItambahApp.swift
@@ -0,0 +1,17 @@
+//
+//  ItambahApp.swift
+//  Itambah
+//
+//  Created by Vicky Irwanto on 03/04/23.
+//
+
+import SwiftUI
+
+@main
+struct ItambahApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
